2564d10d5f60d9064c59c8acc33c0992bcace03a
RomainBoudet <romainoclock@gmail.com>
2021-11-12 19:43:55 +0100

    add possibility to change the cadex

--- app/controllers/cadexController.js
+++ app/controllers/cadexController.js
@@ -1,4 +1,3 @@
-
 const cadexFactory = require('../services/cadexFactory');
 const chalk = require('chalk');
 /**
@@ -6,16 +5,25 @@ const chalk = require('chalk');
  */
 const cadexController = {
 
-/**
- * MW chargé de générer un cadavre exquis
- * L'utilisateur pourra fournir des morceaux dans la query string
- * @param {Express.Request} req 
- * @param {Express.Response} res 
- */
+    /**
+     * MW chargé de générer un cadavre exquis
+     * L'utilisateur pourra fournir des morceaux dans la query string
+     * @param {Express.Request} req 
+     * @param {Express.Response} res 
+     */
     getCadex: (req, res) => {
 
-        console.log(chalk.red`un cadex a été demandé !`);
-        res.status(200).json(cadexFactory.generate().glue());
+        // je génére une copie de mon cadex et req.query peut contenir des morceaux de cadex décidé par l'utilisateur. 
+        // potentiellement un verb, un name, un adjective, un complement
+        // Et si il y a deux clés identiques, la deuxieme écrasse la premiére, celle donné par l'utilisateur prend donc le dessus !
+
+        const baseCadex = {
+            ...cadexFactory.generate(),
+            ...req.query
+        };
+
+        console.log(chalk.red `un cadex a été demandé !`);
+        res.status(200).json(baseCadex.glue());
     }
 
 }
