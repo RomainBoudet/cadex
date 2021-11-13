39c5c13add82da047f79ce16e34131f190c290dc
RomainBoudet <romainoclock@gmail.com>
2021-11-13 13:53:48 +0100

    add swagger doc

--- app.js
+++ app.js
@@ -5,24 +5,32 @@ const fs = require('fs');
 const helmet = require('helmet');
 
 const express = require('express');
-
+const router = require('./app/router');
 const app = express();
-
 const port = process.env.PORT || 4040;
+
+//! Ma doc swagger
+
+const expressSwagger = require('express-swagger-generator')(app);
+let optionSwagger = require('./swagger-config.json');
+optionSwagger.basedir = __dirname; // __dirname désigne le dossier du point d'entrée
+optionSwagger.swaggerDefinition.host = `localhost:${port}`;
+expressSwagger(optionSwagger);
+
+
+//! Mes MW
 
 //helmet : https://expressjs.com/fr/advanced/best-practice-security.html 
 app.use (helmet()); // => source de [DEP0066] DeprecationWarning: OutgoingMessage.prototype._headers is deprecated
 
-const router = require('./app/router');
-
-// Je permet a mon API de lire les objet JSON rentrant 
+// Je permet a mon API de lire les objets JSON rentrant 
 app.use(express.json());
 // Je permet a mon API de savoir lire les formats urlencoded
 app.use(express.urlencoded());
-
+// Je préfixe mes routes
 app.use('/api', router);
 
-
+//! Mes options pour le serveur https
 const options = {
     key: fs.readFileSync(process.env.SSL_KEY_FILE),
     cert: fs.readFileSync(process.env.SSL_CERT_FILE),
--- app/router.js
+++ app/router.js
@@ -5,8 +5,28 @@ const cadexController = require('./controllers/cadexController');
 
 const router = Router();
 
+/**
+ * Génère un Cadex de manière aléatoire, avec la possibilité de fournir des segments de phrase
+ * @route GET /cadex
+ * @group Cadex - génération de cadavre exquis
+ * @param {string} name.query - le nom qu'on peut fournir
+ * @param {string} adjective.query - l'adjectif qu'on peut fournir
+ * @param {string} verb.query - le verbe qu'on peut fournir
+ * @param {string} complement.query - le complément qu'on peut fournir
+ * @returns {string} 200 - le cadavre exquis généré
+ */
 router.get('/cadex', cadexController.getCadex);
 
+/**
+ * Ajoute des propositions de segments de phrase et génère un Cadex basé sur les segments fournis
+ * @route POST /cadex
+ * @group Cadex - génération de cadavre exquis
+ * @param {string} name.body - le nom qu'on peut fournir
+ * @param {string} adjective.body - l'adjectif qu'on peut fournir
+ * @param {string} verb.body - le verbe qu'on peut fournir
+ * @param {string} complement.body - le complément qu'on peut fournir
+ * @returns {string} 200 - le cadavre exquis généré
+ */
 router.post('/cadex', cadexController.addproposition);
 
 module.exports = router;
